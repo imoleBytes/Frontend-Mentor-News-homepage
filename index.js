0ed21a7b7e72c5f057d8df0cab29486fd0f66371
imole <dremkay71@gmail.com>
2023-01-28 15:15:30 +0100

    prepared to launch

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const navBar = document.querySelector('.navbar')
+const navBar = document.querySelector('.navbar');
+const container = document.querySelector('.container');
+
 
 
 function openMenu() {
     // console.log(navBar)
     navBar.style.visibility = 'visible';
+    // container.classList.add('overlay');
+
 }
 
 function closeMenu() {
     // console.log(navBar)
     navBar.style.visibility = 'hidden';
+    // container.classList.remove('overlay');
 }

c3e2eac24d19ace41e45759c0d168e899c916a5b
imole <dremkay71@gmail.com>
2023-01-28 14:33:41 +0100

    js done

---
+++ index.js
@@ -0,0 +1,12 @@
+const navBar = document.querySelector('.navbar')
+
+
+function openMenu() {
+    // console.log(navBar)
+    navBar.style.visibility = 'visible';
+}
+
+function closeMenu() {
+    // console.log(navBar)
+    navBar.style.visibility = 'hidden';
+}
